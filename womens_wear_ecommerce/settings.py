5c060aa31de4c7ffaf318349225e31994497f6ec
AbdurrabKhan198 <abdurrabkhan709@gmail.com>
2025-10-13 21:30:58 +0530

    design change

--- womens_wear_ecommerce/settings.py
+++ womens_wear_ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['kingdupattahouse.in', 'www.kingdupattahouse.in', '3.109.208.181', 'localhost', '127.0.0.1', 'testserver'] # Allow all hosts for development
